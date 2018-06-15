0594b5f872d97a7fc55364a7e423530c2b931427
Samusev Trofim <trofa13@inbox.ru>
2018-06-15 12:40:07 +0300

    added linked list implementation

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,167 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head  = new Node(data, this.head);
+    }
+
+    size() {
+        let size = 0;
+        let node = this.head;
+
+        while (node) {
+            node = node.next;
+            size++;
+        }
+
+        return size;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+
+            node = node.next;
+        }
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        const lastNode = this.getLast();
+
+        if (!lastNode) {
+            this.head = new Node(data);
+            return;
+        }
+        
+        lastNode.next = new Node(data);
+    }
+
+    getAt(idx) {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            if (counter === idx) {
+                return node;
+            }
+
+            counter++;
+            node = node.next;
+        }
+        
+        return null;
+    }
+
+    removeAt(idx) {
+        if (!this.head) {
+            return;
+        }
+
+        if (idx === 0) {
+            this.head = this.head.next;
+        }
+
+        const previous = this.getAt(idx - 1);
+
+        if (!previous || !previous.next) {
+            return;
+        }
+
+        previous.next = previous.next.next;
+    }
+
+    insertAt(data, idx) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (idx === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const previous = this.getAt(idx - 1) || this.getLast();
+        const node = new Node(data, previous.next);
+        previous.next = node;
+    }
+
+    forEach(cb) {
+        if (!this.head) {
+            return;
+        }
+
+        let node = this.head;
+        while (node) {
+            cb(node);
+            node = node.next;
+        }
+    }
+
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (node) {
+            yield node;
+            node = node .next;
+        }
+    }
+}
 
 module.exports = { Node, LinkedList };
--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+    }
+
+    add(item) {
+        this.stackA.push(item);
+    }
+
+    remove() {
+        while (this.stackA.peek()) {
+            this.stackB.push(this.stackA.pop());
+        }
+        
+        const itemToReturn = this.stackB.pop();
+        
+        while (this.stackB.peek()) {
+            this.stackA.push(this.stackB.pop());
+        }
+
+        return itemToReturn;
+    }
+
+    peek() {
+        while (this.stackA.peek()) {
+            this.stackB.push(this.stackA.pop());
+        }
+        
+        const itemToReturn = this.stackB.peek();
+        
+        while (this.stackB.peek()) {
+            this.stackA.push(this.stackB.pop());
+        }
+
+        return itemToReturn;
+    }
+}
 
 module.exports = Queue;
